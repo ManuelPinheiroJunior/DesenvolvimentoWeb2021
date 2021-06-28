6f90ea61e4b0becc5d191a3e33e156ee01720d73
Manuel da Costa Pinheiro Junior <manuel.jr@outlook.com>
2021-06-28 14:03:30 -0300

    commit full

---
+++ 4 - JavaScript/Códigos/Estruturas básicas e Algoritmos/meu_script.js
@@ -0,0 +1,10 @@
+//alert('Olá, seja bem vindo ao curso!!!')
+
+/*
+	seleciona um elemento no DOM
+	atualizar o valor desse elemento com uma string
+
+*/
+
+document.getElementById('nome').value = 'Oi'
+
